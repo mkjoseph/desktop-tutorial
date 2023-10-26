1ccc153c7f3d2a4da5bafd68c6e399c38c65cc6e
mkjoseph <126701784+mkjoseph@users.noreply.github.com>
2023-10-26 09:54:03 -0400

    Update import spotipy.py

--- import spotipy.py
+++ import spotipy.py
@@ -1,8 +1,12 @@
 import spotipy
 import spotipy.util as util
-import random
+import os
+import sys
+import subprocess
+import json
+import requests
 
-# Set up authentication with Spotify API
+# Authenticate with the Spotify API
 username = 'your_username'
 scope = 'playlist-modify-public'
 client_id = 'your_client_id'
@@ -10,34 +14,46 @@ client_secret = 'your_client_secret'
 redirect_uri = 'http://localhost:8888/callback'
 token = util.prompt_for_user_token(username, scope, client_id, client_secret, redirect_uri)
 
-# Get the track IDs of all the tracks in the playlist
-sp = spotipy.Spotify(auth=token)
-playlist_id = 'your_playlist_id'
-results = sp.playlist_tracks(playlist_id)
-tracks = results['items']
+# Create a new playlist in the user's Spotify account
+if token:
+    sp = spotipy.Spotify(auth=token)
+    playlist_name = 'iTunes Playlist'
+    playlist_description = 'Playlist imported from iTunes'
+    sp.user_playlist_create(username, playlist_name, public=True, description=playlist_description)
+    playlists = sp.user_playlists(username)
+    playlist_id = None
+    for playlist in playlists['items']:
+        if playlist['name'] == playlist_name:
+            playlist_id = playlist['id']
+            break
 
-# Sort the tracks based on the number of plays
-tracks.sort(key=lambda x: x['track']['popularity'], reverse=True)
+    # Get the tracks from the user's iTunes library
+    itunes_library_path = 'C:/Users/your_username/Music/iTunes/iTunes Music Library.xml'
+    if not os.path.exists(itunes_library_path):
+        print('iTunes library not found')
+        sys.exit(1)
+    itunes_library_xml = subprocess.check_output(['plutil', '-convert', 'json', '-o', '-', itunes_library_path])
+    itunes_library = json.loads(itunes_library_xml)
 
-# Get the track IDs in the sorted order
-track_ids = [track['track']['id'] for track in tracks]
+    # Search for each track on Spotify using the Spotipy library
+    for track in itunes_library['Tracks'].values():
+        if 'Location' not in track:
+            continue
+        location = track['Location']
+        if not location.startswith('file:///'):
+            continue
+        filepath = location[7:].replace('/', '\\')
+        if not os.path.exists(filepath):
+            continue
+        title = track['Name']
+        artist = track['Artist']
+        query = f'track:{title} artist:{artist}'
+        results = sp.search(q=query, type='track')
+        if len(results['tracks']['items']) == 0:
+            continue
+        track_uri = results['tracks']['items'][0]['uri']
 
-# Create a new playlist to append the sorted playlist to
-new_playlist_name = 'Sorted Playlist'
-sp.user_playlist_create(username, new_playlist_name)
-
-# Get the ID of the new playlist
-playlists = sp.user_playlists(username)
-new_playlist_id = None
-for playlist in playlists['items']:
-    if playlist['name'] == new_playlist_name:
-        new_playlist_id = playlist['id']
-        break
-
-# Add the sorted tracks to the new playlist using the playlist_add_items() function
-sp.playlist_add_items(new_playlist_id, track_ids)
-
-# Print a success message
-print('Playlist sorted and appended to new playlist successfully!')
-
-
+        # Add each track to the new playlist in the user's Spotify account
+        sp.user_playlist_add_tracks(username, playlist_id, [track_uri])
+else:
+    print('Authentication failed')
