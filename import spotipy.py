7c309b66656e982ccc8fceb04576cf9ca1575f67
mkjoseph <126701784+mkjoseph@users.noreply.github.com>
2023-10-26 09:52:54 -0400

    Update import spotipy.py

--- import spotipy.py
+++ import spotipy.py
@@ -10,18 +10,20 @@ client_secret = 'your_client_secret'
 redirect_uri = 'http://localhost:8888/callback'
 token = util.prompt_for_user_token(username, scope, client_id, client_secret, redirect_uri)
 
-
 # Get the track IDs of all the tracks in the playlist
 sp = spotipy.Spotify(auth=token)
+playlist_id = 'your_playlist_id'
 results = sp.playlist_tracks(playlist_id)
 tracks = results['items']
+
+# Sort the tracks based on the number of plays
+tracks.sort(key=lambda x: x['track']['popularity'], reverse=True)
+
+# Get the track IDs in the sorted order
 track_ids = [track['track']['id'] for track in tracks]
 
-# Shuffle the track IDs using the random.shuffle() function
-random.shuffle(track_ids)
-
-# Create a new playlist to append the shuffled playlist to
-new_playlist_name = 'Shuffled Playlist'
+# Create a new playlist to append the sorted playlist to
+new_playlist_name = 'Sorted Playlist'
 sp.user_playlist_create(username, new_playlist_name)
 
 # Get the ID of the new playlist
@@ -32,8 +34,10 @@ for playlist in playlists['items']:
         new_playlist_id = playlist['id']
         break
 
-# Add the shuffled tracks to the new playlist using the playlist_add_items() function
+# Add the sorted tracks to the new playlist using the playlist_add_items() function
 sp.playlist_add_items(new_playlist_id, track_ids)
 
 # Print a success message
-print('Playlist shuffled and appended to new playlist successfully!')
+print('Playlist sorted and appended to new playlist successfully!')
+
+
