421b58c4e4c7d6c5a9b320c3818723c46207b0d6
mkjoseph <126701784+mkjoseph@users.noreply.github.com>
2023-10-27 09:31:46 -0400

    Update item.js

---
+++ item.js
@@ -0,0 +1,11 @@
+const num1 = 2;
+const num2 = 3;
+const num3 = 4;
+const num4 = 5;
+const num5 = 6;
+const num6 = 7;
+const num7 = 8;
+
+const result = num1 * num2 * num3 * num4 * num5 * num6 * num7;
+
+console.log(result);
