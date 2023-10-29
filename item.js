dcc1d157ddd746e2d80b6c4a593598be38d2a23f
mkjoseph <126701784+mkjoseph@users.noreply.github.com>
2023-10-29 16:03:43 -0400

    Update item.js

--- item.js
+++ item.js
@@ -1,14 +1,16 @@
 function nextInLine(arr, item) {
     // Only change code below this line
-    
+
     return item;
     // Only change code above this line
-  }
-  
-  // Setup code
-  let testArr = [1, 2, 3, 4, 5];
-  
-  // Display code
-  console.log("Before: " + JSON.stringify(testArr));
-  console.log(nextInLine(testArr, 6));
-  console.log("After: " + JSON.stringify(testArr));
+}
+
+// Setup 
+let testArr = [1, 2, 3, 4, 5];
+
+
+
+// Display code
+console.log(nextInLine(testArr, 6));
+console.log("Before: " + JSON.stringify(testArr));
+console.log("After: " + JSON.stringify(testArr));
