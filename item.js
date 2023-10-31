b19ad67145a57a5dd428d70719acf0b5bb9dc065
mkjoseph <126701784+mkjoseph@users.noreply.github.com>
2023-10-31 12:02:48 -0400

    Update item.js

--- item.js
+++ item.js
@@ -1,11 +1,21 @@
-const num1 = 2;
-const num2 = 3;
-const num3 = 4;
-const num4 = 5;
-const num5 = 6;
-const num6 = 7;
-const num7 = 8;
-
-const result = num1 * num2 * num3 * num4 * num5 * num6 * num7;
-// 
-console.log(result);
+function chainToSwitch(val) {
+    let answer = "";
+    // Only change code below this line
+  
+    if (val === "bob") {
+      answer = "Marley";
+    } else if (val === 42) {
+      answer = "The Answer";
+    } else if (val === 1) {
+      answer = "There is no #1";
+    } else if (val === 99) {
+      answer = "Missed me by this much!";
+    } else if (val === 7) {
+      answer = "Ate Nine";
+    }
+  
+    // Only change code above this line
+    return answer;
+  }
+  
+  chainToSwitch(7);
